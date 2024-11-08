1f86beeedb6eb7507fc83219506ecd3d4b38938b
Damian Gatta <damian.gatta@student.uibk.ac.at>
2024-11-08 21:36:18 +0100

    Add map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,31 @@
+<script>
+    import { MapLibre, DefaultMarker, Popup } from "svelte-maplibre";
+    import { onMount } from "svelte";
+
+    let markers = [
+        { lngLat: [-20, 0], name: "Africa" },
+        { lngLat: [0, 0], name: "Prime Meridian" },
+        { lngLat: [20, 0], name: "Africa" },
+    ];
+
+    let mapClasses = "w-full h-96";
+</script>
+
 <h1 class="text-2xl text-center p-12">Welcome to MedRide</h1>
+<MapLibre
+    style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+    class={mapClasses}
+    standardControls
+    zoom={1}
+    center={[-20, 0]}
+>
+    {#each markers as { lngLat, name }}
+        <!-- Unlike the custom marker example, default markers do not have mouse events,
+    and popups only support the default openOn="click" behavior -->
+        <DefaultMarker {lngLat} draggable>
+            <Popup offset={[0, -10]}>
+                <div class="text-lg font-bold">{name}</div>
+            </Popup>
+        </DefaultMarker>
+    {/each}
+</MapLibre>
