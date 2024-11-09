5ee1c2e47e045728d1d8de0c62cbf62c39d8d15a
Damian Gatta <damian.gatta@student.uibk.ac.at>
2024-11-09 08:10:33 +0100

    Add trips

--- src/lib/route.js
+++ src/lib/route.js
@@ -40,3 +40,46 @@ export async function getRoute(coordinates) {
     }
     return {duration, route}
 }
+
+export async function getTrip(coordinates) {
+    // Construct the base URL
+    const baseUrl = "http://127.0.0.1:5000/trip/v1/driving/";
+
+    let duration = 0;
+    let route = {
+        type: "Feature",
+        geometry: null,
+        properties: {}
+    };
+
+    // Format coordinates as required by the API
+    const coordinatesParam = coordinates.map(coord => coord.join(',')).join(';');
+    // Construct the full URL with query parameters
+    const url = `${baseUrl}${coordinatesParam}?geometries=geojson`;
+    try {
+        const response = await fetch(url);
+        // Check if the response is OK
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+        // Parse and return JSON data
+        const data = await response.json();
+        let duration = data.routes[0].duration;
+        route.geometry = geometry;
+        return {duration, route};
+    } catch (error) {
+        console.error("Failed to fetch route:", error);
+        return null;
+    }
+}
+
+export async function getTripWithEnd(coordinates, end) {
+    let {duration, route} = await getTrip(coordinates);
+    let endPoint = route.geometry.coordinates[route.geometry.coordinates.length - 1];
+    let {duration2, route2} = await getRoute([endPoint, end]);
+    duration += duration2;
+    route.geometry.coordinates = route.geometry.coordinates.concat(route2.geometry.coordinates);
+    return {duration, route}
+}
+
+
