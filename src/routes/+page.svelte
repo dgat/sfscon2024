8b10ea8391b3aa4ee391ceed21a0f854e5a7c9ec
DG <gattadg@gmail.com>
2024-11-09 11:55:31 +0100

    Autoscheduling works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,6 +65,15 @@
             return;
         }
 
+        if (
+            transport.tasks.length == 1 &&
+            transport.tasks[0].startPlace == transport.tasks[0].endPlace
+        ) {
+            focusedTransport = transport;
+            geometry = null;
+            return;
+        }
+
         let coordinates = [];
         transport.tasks.forEach((t) => coordinates.push(t.startCoordinates));
         transport.tasks.forEach((t) => coordinates.push(t.endCoordinates));
@@ -102,6 +111,16 @@
         let transport = new TP(vehicleType, [...selectedTasks]);
         scheduledTransports.push(transport);
         selectedTasks = [];
+    }
+
+    function autoSchedule() {
+        [...tasks].forEach((t) => {
+            if (t.type == "LIEGE" && t.isDate("03.01.2020")) {
+                vehicleType = "KTW";
+                selectMarker(t);
+                scheduleTransport();
+            }
+        });
     }
 </script>
 
@@ -234,9 +253,38 @@
                 </div>
             </div>
         </div>
-        <div class="transport-section px-4">
-            <p class="text-lg font-semibold pb-3">Scheduled Transports</p>
-            <div class="flex flex-col gap-2">
+        <div
+            class="transport-section px-4 flex flex-col max-h-full overflow-scroll"
+        >
+            <div class="flex align-middle justify-between">
+                <p class="text-lg font-semibold pb-3">Scheduled Transports</p>
+                <button
+                    class="btn btn-sm inline-block"
+                    aria-label="Automatic Schedule"
+                    onclick={autoSchedule}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="size-6"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M21.75 6.75a4.5 4.5 0 0 1-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 1 1-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 0 1 6.336-4.486l-3.276 3.276a3.004 3.004 0 0 0 2.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852Z"
+                        />
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M4.867 19.125h.008v.008h-.008v-.008Z"
+                        />
+                    </svg>
+                </button>
+            </div>
+            <div class="flex flex-col gap-2 overflow-scroll flex-1">
                 {#each scheduledTransports as transport}
                     <Transport
                         {transport}
