619a1a8e0eaabad8a3ef024312e647da139d3a77
Damian Gatta <damian.gatta@student.uibk.ac.at>
2024-11-09 07:46:57 +0100

    fix link

--- setup_osrm.sh
+++ setup_osrm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://download.geofabrik.de/europe/italy/nord-est.html
+wget https://download.geofabrik.de/europe/italy/nord-est-latest.osm.pbf
 sudo docker run -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-extract -p /opt/car.lua /data/nord-est.osm.pbf || echo "osrm-extract failed"
 sudo docker run -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-partition /data/nord-est-latest.osrm || echo "osrm-partition failed"
 sudo docker run -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-customize /data/nord-est-latest.osrm || echo "osrm-customize failed"
